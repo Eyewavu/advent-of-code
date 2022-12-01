d35c791e64b1f485656817de7a3a557e92d38570
Eyewavu <ew.music.yt@gmail.com>
2022-12-01 16:34:52 +0100

    setup

--- script.sh
+++ script.sh
@@ -1,6 +1,22 @@
+#!/bin/bash
+
+function create_link () {
+  echo "$1"
+  ln -s "../$1"
+}
+
+rm -fr day-*
+mkdir -p day-{01..25}
+
 for i in ./day-*/
 do
   cd "$i"
+  cp ../package.template.json package.json
   yarn init -y
-  cd..
+  create_link 'tsconfig.json'
+  create_link 'rollup.config.js'
+  yarn install
+  mkdir src
+  touch src/index.ts
+  cd ..
 done
